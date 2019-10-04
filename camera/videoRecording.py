83a8de4934cf90c4320869336246208443786721
Yiling <liuyiling33@hotmail.com>
2019-10-04 11:24:29 +0200

    dscfsfc

--- camera/videoRecording.py
+++ camera/videoRecording.py
@@ -1,2 +1,2 @@
 import os
-os.system('ffmpeg -t 30 -f v4l2 -framerate 25 -video_size 640x80 -i /prototyping/camera -t 300 output.mkv')
+os.system('ffmpeg -t 30 -f v4l2 -framerate 25 -video_size 640x80 -i /prototype/camera/video0 -t 300 output.mkv')
