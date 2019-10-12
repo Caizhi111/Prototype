3dfbe0f379420dfeb222a781bafd7f1943e14825
Yufei He <yufei0he@gmail.com>
2019-10-12 21:34:02 +0200

    video

--- camera/videoRecording.py
+++ camera/videoRecording.py
@@ -1,2 +1,29 @@
-import os
-os.system('ffmpeg -t 30 -f v4l2 -framerate 25 -video_size 640x80 -i /prototype/camera/ -t 300 output.mkv')
+# yiling
+#import os
+#os.system('ffmpeg -t 30 -f v4l2 -framerate 25 -video_size 640x80 -i /prototype/camera/ -t 300 output.mkv')
+
+#yufei
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
