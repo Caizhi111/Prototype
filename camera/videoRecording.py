757e64a4e2f33b8ea895ca1d8df41d91d52cceab
Yufei He <yufei0he@gmail.com>
2019-10-14 11:21:15 +0200

    camera test

--- camera/videoRecording.py
+++ camera/videoRecording.py
@@ -5,21 +5,55 @@
 #yufei
 import numpy as np
 import cv2
+import time
+import os
+import random
+import sys
+
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+name = random.randint(0, 1000)
+print(name)
+if os.path.isdir(str(name)) is False:
+    name = random.randint(0, 1000)
+    name = str(name)
+
+name = os.path.join(os.getcwd(), str(name))
+print("ALl logs saved in dir:", name)
+os.mkdir(name)
+
 
 cap = cv2.VideoCapture(1)
+cur_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
-while(cap.isOpened()):
+start = time.time()
+video_file_count = 1
+video_file = os.path.join(name, str(video_file_count) + ".avi")
+print("Capture video saved location : {}".format(video_file))
+
+# Create a video write before entering the loop
+out = cv2.VideoWriter(video_file, fourcc, 20, (640, 480))
+
+while cap.isOpened():
+    start_time = time.time()
     ret, frame = cap.read()
-    if ret==True:
-        frame = cv2.flip(frame,0)
+    if ret == True:
+        cv2.imshow("frame", frame)
+        if time.time() - start > 10: #Every ten seconds save the video in created directory file
+            start = time.time()
+            video_file_count = 1
+            video_file = os.path.join(name, str(video_file_count) + ".avi")
+            out = cv2.VideoWriter(video_file, fourcc, 20, (640, 480))
+            # No sleeping! We don't want to sleep, we want to write
+            # time.sleep(10)
 
+        # Write the frame to the current video writer
         out.write(frame)
-
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         break
