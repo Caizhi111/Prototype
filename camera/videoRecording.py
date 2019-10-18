55d8578ad9c8c0b1d6131c372844ac3131cd5369
Yufei He <yufei0he@gmail.com>
2019-10-18 15:21:21 +0200

    1

--- camera/videoRecording.py
+++ camera/videoRecording.py
@@ -49,7 +49,7 @@ while cap.isOpened():
         if time.time() - start > 10: #Every ten seconds save the video in created directory file
             start = time.time()
 
-            video_file = os.path.join("video", "1" + ".mp4")
+            video_file = os.path.join("home", "pi", "Prototype", "video", "1" + ".mp4")
             out = cv2.VideoWriter(video_file, fourcc, 24, (640, 480))
             # No sleeping! We don't want to sleep, we want to write
             # time.sleep(10)
