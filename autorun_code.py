151a6b2355a6fef3f8aa45adf0f3083a4c882926
Yiling <liuyiling33@hotmail.com>
2019-10-24 14:28:46 +0200

    efsfds

--- autorun_code.py
+++ autorun_code.py
@@ -5,7 +5,7 @@ from time import sleep
 y=(0.1)
 subprocess.Popen(["python", 'serial_to_dcdhub2.py'])
 sleep(y)
-subprocess.Popen(["python", 'random-data.py'])
+subprocess.Popen(["python", 'videoRecording.py'])
 #sleep (y)
 #subprocess.Popen(["python", '3.py'])
 #sleep (y)
