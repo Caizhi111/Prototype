15ed4abfa051ad23143551e1e01dcd81e77fa9a6
Yufei He <yufei0he@gmail.com>
2019-10-25 09:45:55 +0200

    dfsgs

--- autorun_code.py
+++ autorun_code.py
@@ -3,6 +3,10 @@ import subprocess
 from time import sleep
 
 y=(0.1)
-subprocess.Popen(["python", 'serial_to_dcdhub2.py'])
+subprocess.Popen(["python", '1.py'])
 sleep(y)
-subprocess.Popen(["python", 'videoRecording.py'])
+subprocess.Popen(["python", '2.py'])
+sleep (y)
+subprocess.Popen(["python", '3.py'])
+sleep (y)
+subprocess.Popen(["python", '4.py'])
