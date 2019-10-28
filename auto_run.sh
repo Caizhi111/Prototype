d02505a7f76a7a9185e710ce83b3d4e723e07241
Yiling33 <liuyiling33@hotmail.com>
2019-10-28 15:50:57 +0100

    hi

--- auto_run.sh
+++ auto_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 serial_to_dcdhub2.py &
-python3 camera/videoRecording.py &
-python3 web/static/wechat_sending.py &
-python3 web/server.py &
+python3 /home/pi/Prototype/serial_to_dcdhub2.py &
+python3 /home/pi/Prototype/camera/videoRecording.py &
+python3 /home/pi/Prototype/web/static/wechat_sending.py &
+python3 /home/pi/Prototype/web/server.py &
