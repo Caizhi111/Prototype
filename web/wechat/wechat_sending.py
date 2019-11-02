c57cb940bde24df37580bc8a93bb99610ab931b3
zhicai <a1145371578@gmail.com>
2019-11-02 15:03:38 +0100

    sdw

--- web/wechat/wechat_sending.py
+++ web/wechat/wechat_sending.py
@@ -242,6 +242,6 @@ while(True):
         itchat.send(message_content_2, toUserName = contact_person)
         itchat.send(message_content_3, toUserName = contact_person)
     #itchat.send(message_content, toUserName = contact_person)
-
         itchat.run()
+        break
     sleep(1)
